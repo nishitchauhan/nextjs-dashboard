21823acab910e8ab01fd0ec7fda6e7b483ceceb8
Nishit <nishit@Nishits-Mac-Pro.local>
2025-02-27 11:01:44 +0530

    next js example

---
+++ app/ui/font.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
+export const lusitana_700 = Lusitana({ subsets: ["latin"], weight: "700" });
